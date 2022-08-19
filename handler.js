2a5192a2b3baa55b5f3f65e86c9ed3dd394fca0d
jammeijerink <julian.meijerink@gmail.com>
2022-08-19 14:46:39 +0200

    Add AWS Express server

--- handler.js
+++ handler.js
@@ -1,23 +1,9 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./src/index');
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
+const server = awsServerlessExpress.createServer(app);
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
+exports.handler = (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+}
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-module.exports.handler = serverless(app);
