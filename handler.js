39e25a74a57224c708d9d0edd4a654a409fb4e1c
jammeijerink <julian.meijerink@gmail.com>
2022-08-22 14:16:53 +0200

    Add comments

--- handler.js
+++ handler.js
@@ -1,8 +1,13 @@
+// AWS serverless express server used for this project 
 const awsServerlessExpress = require('aws-serverless-express');
+
+// Actual app where Express framework and endpoints are
 const app = require('./src/Aws');
 
+// Create server
 const server = awsServerlessExpress.createServer(app);
 
+// Export created server, as a response to the serverless.yml file
 exports.handler = (event, context) => {
   return awsServerlessExpress.proxy(server, event, context);
 }
--- src/Aws.js
+++ src/Aws.js
@@ -2,18 +2,21 @@
 const express = require("express");
 const app = express();
 
+// Root endpoint
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
 });
 
+// Basic /hello GET endpoint
 app.get("/hello", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
 });
 
+// For all other endpoints return not
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
