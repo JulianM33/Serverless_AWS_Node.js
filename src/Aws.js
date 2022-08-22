dc0564fdd5b937d60de67250c3ca23f46cd6570d
jammeijerink <julian.meijerink@gmail.com>
2022-08-22 14:34:59 +0200

    Add POST Request

--- src/Aws.js
+++ src/Aws.js
@@ -16,6 +16,21 @@ app.get("/hello", (req, res, next) => {
   });
 });
 
+// Basic POST request
+app.post("/",  (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello Post",
+  });
+});
+
+// The '*' is a REGEX character, all request beginning with 'ab' 
+// and ending in 'cd' will connect to this endpoint
+app.get('/ab*cd',(req, res, next) => {
+  return res.status(200).json({
+    message: "Endpoint beginning in \'ab\' and ending in \'cd\'",
+  });
+});
+
 // For all other endpoints return not
 app.use((req, res, next) => {
   return res.status(404).json({
